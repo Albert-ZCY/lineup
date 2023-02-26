ea6b3200c1b0a75948e6669bf93269444eb745c0
Albert钟 <3378491536@qq.com>
2023-02-26 14:08:20 +0800

    new v

--- lineup.js
+++ lineup.js
@@ -3,7 +3,7 @@ import * as Compile from './compile.js';
 var compile = Compile.compile;
 
 function compileDocument() {
-    let eles = document.getElementsByTagName('markup');
+    let eles = document.getElementsByTagName('lineup');
     for (var i = 0; i < eles.length; i++) {  //遍历数组
         let ele = eles[i];
         let code = ele.innerHTML;
